a10cb45c256c4f342f340b378a76fcbeadaadc85
Roman <roman.qwonix@gmail.com>
2023-02-09 12:49:06 +0300

    add equals override

--- src/main/java/ru/qwonix/tgMoviePlayerBot/entity/Video.java
+++ src/main/java/ru/qwonix/tgMoviePlayerBot/entity/Video.java
@@ -5,6 +5,8 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+import java.util.Objects;
+
 @ToString
 @Builder(toBuilder = true)
 @Getter
@@ -18,4 +20,17 @@ public class Video {
     private int priority;
 
     private boolean hasSubtitles = subtitlesLanguage == null;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Video video = (Video) o;
+        return Objects.equals(videoFileId, video.videoFileId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(videoFileId);
+    }
 }
